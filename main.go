242a3fb6ca45e1ab8853da6c062eb99a62884418
Nick Cloutier <nicknailers69@gmail.com>
2023-07-09 13:44:37 -0400

    start nn

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package qfoxlibs
 
 import (
 	"fmt"
-	"github.com/nicknailers69/qfoxlibs/pkg/nn"
 	_ "github.com/nicknailers69/qfoxlibs/pkg/nn"
 )
 
-func main() {
-	fmt.Print("main test qfoxlibs")
+func init() {
+	fmt.Print("main qfoxlibs")
 
-}
-
-func RunNN() {
-	nn.Run()
 }
